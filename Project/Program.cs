982f9ed361cba384f468e672cf2ba8fe475ed373
Gorets147 <mih.kom1998@gmail.com>
2023-09-13 20:36:48 +0300

    feat: add alghoritm for task

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+PrintArray(array);
+System.Console.WriteLine("\n");
+PrintArray(NewArrayThriChar(array));
+
+
+
+void PrintArray(string[] matr){
+    for (int i = 0; i < matr.Length; i++)
+    {
+        System.Console.Write($"({matr[i]}), ");
+    }
+}
+
+string[] NewArrayThriChar(string[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3){
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3){
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
